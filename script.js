0e324b53d68433fadf5f4b91f493ebe72b99b4f3
shwet1808 <shwetkumar29@gmail.com>
2024-04-04 03:57:12 +0530

    html and css done

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let music = new Audio("background.mp3")
+let turn = new Audio("stop.mp3")
+let gameover = new Audio("gameover.mp3")
+let Turn = "x"
+
+// to  change turn 
+const changeTurn = ()=>{
+    return turn === "x"?"0":"x"
+}
+
+// to cheak for win
+const cheakwin = ()=>{
+
+} 
+
