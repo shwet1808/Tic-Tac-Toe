cfaef5fbe701cfb49eda85ea9ff9d6e35074c4a2
shwet1808 <shwetkumar29@gmail.com>
2024-04-08 02:02:39 +0530

    ..

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 
 
 let music = new Audio("background.mp3")
-let turn = new Audio("stop.mp3")
+let audioturn = new Audio("stop.mp3")
 let gameover = new Audio("gameover.mp3")
-let Turn = "x"
+let turn = "x"
 
 // to  change turn 
-const changeTurn = ()=>{
-    return turn === "x"?"0":"x"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
 
 // to cheak for win
-const cheakwin = ()=>{
+const cheakwin = () => {
 
-} 
+}
 
+//game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=> {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioturn.play();
+            cheakwin();
+            document.getElementsByClassName("info")[0].innerText ="turn for " + turn;
+            
+            
+        }
+
+    })
+
+})
