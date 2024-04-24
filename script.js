7e0cbb98b8013ee5a85dba4cbbd18b0c01182d2b
shwet1808 <shwetkumar29@gmail.com>
2024-04-25 03:07:30 +0530

    no error

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ let music = new Audio("background.mp3")
 let audioturn = new Audio("stop.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "x"
+let isgameover = false;
+
 
 // to  change turn 
 const changeTurn = () => {
-    return turn === "X" ? "0" : "X"
+    return turn === "x" ? "0" : "x"
 }
 
 // to cheak for win
 const cheakwin = () => {
-
+let boxtext = document.getElementsByClassName('boxtext');
+let wins = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6],
+]
+wins.forEach(e =>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + " won"
+    isgameover = true
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+    }
+ 
+})
 }
 
 //game logic
@@ -25,7 +45,12 @@ Array.from(boxes).forEach(element => {
             turn = changeTurn();
             audioturn.play();
             cheakwin();
-            document.getElementsByClassName("info")[0].innerText ="turn for " + turn;
+
+            if(!isgameover){
+
+                document.getElementsByClassName("info")[0].innerText ="turn for " + turn;
+
+            }
             
             
         }
